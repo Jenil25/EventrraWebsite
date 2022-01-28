e8ff43207f1bd154cec91d38153d4526e4d9d695
Jenil25 <jenilmahy25@gmail.com>
2022-01-28 11:53:09 +0530

    Initial Commit

---
+++ my-app/src/Admin/Home.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react'
+// import axios from 'axios'
+// import { getAllDemoFile } from '../functions'
+// import { NavLink, useHistory } from 'react-router-dom'
+
+const Home = () => {
+
+    // const [demo, setDemo] = useState([""]);
+    // useEffect(() => {
+    //     const getDemo = async () => {
+    //         const res = await axios.post("https://eventrra.000webhostapp.com/getAllDemoFile.php");
+    //         setDemo(res.data);
+    //     }
+    //     getDemo();
+    // }, []);
+    
+    // if (demo) {
+    //     var demoData = demo.map((val, i) => (
+    //         <tr key={i}>
+    //             <td key={val._id} className="clm1">{i + 1}</td>
+    //             <td className="clm2">{val.Row1}</td>
+    //             <td className="clm3">{val.Row2}</td>
+    //             <td className="clm4">{val.Row3}</td>
+    //             <td className="clm5"><button className="btn btn-primary">Edit</button>&ensp;
+    //                 <button className="btn btn-danger">Delete</button></td>
+    //         </tr>
+    //     ))
+   
+    // } else {
+    //     demoData = <tr>
+    //         <td colSpan="4">No Records Found</td>
+    //     </tr>
+    // }
+
+    return(
+        // <div>
+        //     <br/><br/><center><h1>All demo</h1></center>
+        //     <div><br/><br/><center>
+        //         <table bordered className="table-city">
+        //             <thead>
+        //                 <tr>
+        //                     <th className="clm1">#</th>
+        //                     <th className="clm2">Row1</th>
+        //                     <th className="clm3">Row2</th>
+        //                     <th className="clm4">Row3</th>
+        //                     <th className="clm5">Action</th>
+        //                 </tr>
+        //             </thead>
+        //             <tbody>
+        //                 {/* {demoData} */}
+        //             </tbody>
+        //         </table>
+        //         </center></div>
+        //         <section >
+        //        {/* <center> <NavLink to="/adminpanel" className="back2">Back</NavLink></center> */}
+        //         <br />
+        //     </section>
+        //     <br/><br/><br/>
+        // </div>
+        <div>
+            H;ds,c
+        </div>
+    )
+}
+
+export default Home
---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Home from "./Admin/Home";
+import Home1 from "./Components/demo";
+import { NavLink, useHistory, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <>
+        <Home1 />
+      </>
+    </div>
+  );
+};
+
+export default App;
